87c3b91f06f586280df0ca92aec0d5d39c07b99e
unknown <tom.daudelin@mail.utoronto.ca>
2021-04-01 18:26:12 -0400

    added a last name text file reader and linked it to the search queries

---
+++ src/main/java/LastNameFileReader.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+/**
+	Reads Last names from last name text file with the one line per name format
+*/
+public class LastNameFileReader {
+	
+	private BufferedReader file_reader;
+	
+	//Sets up the buffered reader (links last name file in order to read from it)
+	public LastNameFileReader(String resource_path) throws IOException {
+		String path = System.getProperty("user.dir");
+		path.concat(resource_path);
+		
+		this.file_reader = new BufferedReader(new FileReader(path));
+		this.file_reader.mark(0);
+	}
+	
+	//Read a last name from the last name text file
+	public String readLastName() throws IOException {
+		return this.file_reader.readLine();
+	}
+	
+	//Reset file pointer to allow re-reading the last name file
+	public void reset() throws IOException {
+		this.file_reader.reset();
+	}
+	
+	//Close the buffered reader after completion of reading file
+	public void close() throws IOException {
+		this.file_reader.close();
+	}
+}
--- src/main/java/SampleClient.java
+++ src/main/java/SampleClient.java
@@ -1,27 +1,40 @@
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
+
+import java.io.IOException;
+
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Patient;
 
 public class SampleClient {
-
-    public static void main(String[] theArgs) {
+	
+	static final String LAST_NAME_FILE_PATH = "\\src\\main\\resources\\last_names.txt";
+	
+    public static void main(String[] theArgs) throws IOException {
 
         // Create a FHIR client
         FhirContext fhirContext = FhirContext.forR4();
         IGenericClient client = fhirContext.newRestfulGenericClient("http://hapi.fhir.org/baseR4");
         client.registerInterceptor(new LoggingInterceptor(false));
-
-        // Search for Patient resources
-        Bundle response = client
-                .search()
-                .forResource("Patient")
-                .where(Patient.FAMILY.matches().value("SMITH"))
-                .returnBundle(Bundle.class)
-                .execute();
-
-
+        
+        LastNameFileReader last_name_reader = new LastNameFileReader(LAST_NAME_FILE_PATH);
+        
+        for (int i = 0; i < 3; i++) {
+        	String last_name = last_name_reader.readLastName();
+        	while (last_name != null) {
+        		// Search for Patient resources
+                Bundle response = client
+                        .search()
+                        .forResource("Patient")
+                        .where(Patient.FAMILY.matches().value(last_name))
+                        .returnBundle(Bundle.class)
+                        .execute();
+             }
+        	last_name_reader.reset();
+        }
+    	
+    	last_name_reader.close();
     }
-
+       
 }
